21061c9f1b92a41b381c533c834a1d33fbb8323d
Oliver Hagenauer <oliverhagenauer@gmail.com>
2026-02-22 18:32:16 +0100

    updated flake to be nix shell compatible

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
-  description = "Simple Python 3 development environment with venv and pip support";
+  description =
+    "Simple Python 3 development environment with venv and pip support";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,33 +11,38 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-
-        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
-          pip
-          requests
-          numpy
-        ]);
+        pythonEnv = pkgs.python3.withPackages (ps:
+          with ps; [
+            pip
+            requests
+            numpy
+            pandas
+            scikit-learn
+            matplotlib
+            joblib
+            nbformat
+            pandas-stubs
+            notebook
+          ]);
       in {
+        packages.default = pkgs.buildEnv {
+          name = "python-dev-env";
+          paths = [ pythonEnv ];
+        };
+
         devShells.default = pkgs.mkShell {
           name = "python-dev-shell";
 
-          buildInputs = [ pythonEnv ];
+          buildInputs = [ self.packages.${system}.default ];
 
           shellHook = ''
-            echo "🐍 Python Dev Shell Ready"
+                echo "🐍 Python Dev Shell Ready"
 
-            if [ ! -d ".venv" ]; then
-              echo "📦 Creating virtual environment in .venv"
-              python -m venv .venv
-            fi
 
-            source .venv/bin/activate
-            echo "✅ .venv activated"
-            echo "📦 Python version: $(python --version)"
-            echo "📦 Pip location: $(which pip)"
-
-            # Optional: Uncomment if you want IntelliJ to auto-start
-            pycharm-professional . > /dev/null 2>&1 &
+                if [ -z "''${_IDE_LAUNCHED:-}" ]; then
+                export _IDE_LAUNCHED=1
+            	    nohup setsid pycharm-professional . >/dev/null 2>&1 < /dev/null &
+            	  fi
           '';
         };
       });
